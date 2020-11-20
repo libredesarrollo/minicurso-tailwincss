f3e48003fa06fb14bf076f7ff2a4bb02ef8fe926
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2020-11-20 14:05:54 -0400

    mini curso tailwindcss componentes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '3rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
